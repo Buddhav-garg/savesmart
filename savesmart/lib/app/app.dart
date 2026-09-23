3c16409a680791cdb07c46e474e24c2aaadbda7c
Buddhav Garg <buddhavgarg.31@gmail.com>
2026-09-23 11:58:19 +0530

    Initial Folder skeleton

---
+++ savesmart/lib/app/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SaveSmartApp extends StatelessWidget {
+	const SaveSmartApp({super.key});
+
+	@override
+	Widget build(BuildContext context) {
+		return const MaterialApp(
+			home: Scaffold(
+				body: Center(
+					child: Text('Hello, World!'),
+				),
+			),
+		);
+	}
+}
